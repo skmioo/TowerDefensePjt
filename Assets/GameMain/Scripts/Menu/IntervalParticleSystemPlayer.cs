b0e36ebbf3729c749c61e9db470ed1e5bdb2c1ae
jicongwei <769182096@qq.com>
2022-05-14 08:32:11 +0800

    menu场景搭建

---
+++ Assets/GameMain/Scripts/Menu/IntervalParticleSystemPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDF
+{
+	public class IntervalParticleSystemPlayer : MonoBehaviour
+	{
+		public ParticleSystem ps;
+		public float interval;
+		public DateTime playTime;
+
+		private void Start()
+		{
+			playTime = DateTime.Now.AddSeconds(interval);
+		}
+
+		private void Update()
+		{
+			if (ps != null && playTime <= DateTime.Now)
+			{
+				ps.Play();
+				playTime = DateTime.Now.AddSeconds(interval);
+			}
+		}
+
+
+	}
+}
---
+++ Assets/GameMain/Scripts/Menu/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TowerDF
+{
+	public class Rotator : MonoBehaviour
+	{
+		public Vector3 rotateSpeed;
+		private void Update()
+		{
+			transform.localEulerAngles += rotateSpeed;
+		}
+	}
+}
